7bdff49d16dbdb9b211f6217ed7065b04cb713b0
WhyNotWhyYes <artemiy.zuzin@gmail.com>
2023-04-02 18:36:41 +0300

    fix docker-compose.yml

---
+++ Sources/App/Migrations/CreateSearchTitle.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Artemiy Zuzin on 12.03.2023.
+//
+
+import Foundation
+import Fluent
+
+struct CreateSearchTitle: AsyncMigration {
+    
+    func prepare(on database: Database) async throws {
+        try await database.schema(SearchTitle.schema)
+            .id()
+            .field("title", .string, .required)
+            .field("user_id", .uuid, .required, .references(User.schema, "id"))
+            .field("created_at", .date)
+            .create()
+    }
+    
+    func revert(on database: Database) async throws {
+        try await database.schema(SearchTitle.schema).delete()
+    }
+    
+}
